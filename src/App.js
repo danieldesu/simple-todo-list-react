53ff95c3a2325684d6a844d0e24c6fb08772d83b
Daniel Desu <danieldesuel@gmail.com>
2022-06-11 03:59:53 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [date, setDate] = useState("");
+
+  const handleDelete = (id) => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter((todo) => {
+        return todo.id !== id;
+      });
+    });
+  };
+
+  const formReset = () => {
+    setTodo("");
+    setDate("");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newTodo = {
+      todo: todo,
+      date: date,
+      id: Math.floor(Math.random() * 10000),
+    };
+
+    setTodos([...todos, newTodo]);
+    formReset();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>Hey! Enter your to-dos</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          <span>Enter to-do: </span>
+          <input
+            type="text"
+            value={todo}
+            required
+            maxLength="25"
+            onChange={(e) => {
+              setTodo(e.target.value);
+            }}
+          />
+        </label>
+
+        <label>
+          <span>Enter date: </span>
+          <input
+            type="date"
+            value={date}
+            required
+            onChange={(e) => {
+              setDate(e.target.value);
+            }}
+          />
+        </label>
+        <button>Submit</button>
+      </form>
+      {todos && (
+        <ul>
+          {todos.map((item) => (
+            <li key={item.id}>
+              <span className="todo">{item.todo}</span> Deadline: {item.date}
+              <span className="remove-icon" onClick={() => handleDelete(item.id)}>
+                Remove
+              </span>
+              <hr />
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
